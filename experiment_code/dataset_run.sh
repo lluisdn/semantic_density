54f44d34ed450420e2e77cbe507f32f1df086aed
lluisdn <106274095+lluisdn@users.noreply.github.com>
2024-11-06 18:30:21 +0100

    Update dataset_run.sh

--- experiment_code/dataset_run.sh
+++ experiment_code/dataset_run.sh
@@ -1,4 +1,5 @@
 # huggingface-cli login
+python3 parse_coqa.py
 python3 parse_triviaqa.py --model='Llama-2-13b-hf'
 python3 parse_datasets.py --model='Llama-2-13b-hf' --dataset='Sciq'
 python3 parse_datasets.py --model='Llama-2-13b-hf' --dataset='NQ'

546f6b9fcc4e2110c58d5f1eaf494a332c4df32a
lluisdn <106274095+lluisdn@users.noreply.github.com>
2024-11-07 19:59:28 +0100

    Update dataset_run.sh

--- experiment_code/dataset_run.sh
+++ experiment_code/dataset_run.sh
@@ -1,5 +1,5 @@
 # huggingface-cli login
 python3 parse_coqa.py
 python3 parse_triviaqa.py --model='Mistral-7B-v0.1'
-python3 parse_datasets.py --model='Mistral-7B-v0.1' --dataset='Sciq'
+python3 parse_datasets.py --model='Mistral-7B-v0.1' --dataset='sciq'
 python3 parse_datasets.py --model='Mistral-7B-v0.1' --dataset='NQ'

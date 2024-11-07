cd3c993ea75f8a6b3b4fecb90e32b0abbc212cdf
lluisdn <106274095+lluisdn@users.noreply.github.com>
2024-11-07 20:19:51 +0100

    Update generation_run.sh

--- experiment_code/generation_run.sh
+++ experiment_code/generation_run.sh
@@ -1,4 +1,4 @@
 python3 generate_beam_search_save_all_triviaqa_coqa_cleaned_device.py --num_generations_per_prompt='10' --model='Mistral-7B-v0.1' --fraction_of_data_to_use='0.2' --num_beams='10' --top_p='1.0' --dataset='coqa' --cuda_device=0
 python3 generate_beam_search_save_all_triviaqa_coqa_cleaned_device.py --num_generations_per_prompt='10' --model='Mistral-7B-v0.1' --fraction_of_data_to_use='0.1' --num_beams='10' --top_p='1.0' --dataset='trivia_qa' --cuda_device=0
-python3 generate_beam_search_save_all_datasets_cleaned_device.py --num_generations_per_prompt='10' --model='Mistral-7B-v0.1' --fraction_of_data_to_use='1.0' --num_beams='10' --top_p='1.0' --dataset='sciq' --cuda_device=0
+python3 generate_beam_search_save_all_datasets_cleaned_device.py --num_generations_per_prompt='10' --model='Mistral-7B-v0.1' --fraction_of_data_to_use='1.0' --num_beams='10' --top_p='1.0' --dataset='Sciq' --cuda_device=0
 python3 generate_beam_search_save_all_datasets_cleaned_device.py --num_generations_per_prompt='10' --model='Mistral-7B-v0.1' --fraction_of_data_to_use='0.5' --num_beams='10' --top_p='1.0' --dataset='NQ' --cuda_device=0
